b0b4d9f3676e8f4aa56761f8136ab959160bc525
userror <50100791+Userror101@users.noreply.github.com>
2020-10-11 00:50:32 +0500

    Initial Commit

---
+++ Calculator iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator iOS
+//
+//  Created by userror on 11.10.2020.
+//  Copyright © 2020 Andrey Dubinsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
