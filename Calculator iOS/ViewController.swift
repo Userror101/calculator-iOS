96aadb819806c324bffe2cb769524e77e63ef41f
userror <50100791+Userror101@users.noreply.github.com>
2020-10-14 14:24:00 +0500

    v1

--- Calculator iOS/ViewController.swift
+++ Calculator iOS/ViewController.swift
@@ -1,20 +1,104 @@
-//
-//  ViewController.swift
-//  Calculator iOS
-//
-//  Created by userror on 11.10.2020.
-//  Copyright © 2020 Andrey Dubinsky. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var displayResultLabel: UILabel!
+    var stillTyping = false
+    var dotInPlaced = false
+    var firstOperand: Double = 0
+    var secondOperand: Double = 0
+    var operationSign: String = ""
+    
+    var currentInput: Double {
+        get {
+            return Double(displayResultLabel.text!)!
+        }
+        set {
+            let value = "\(newValue)"
+            let valueArrey = value.components(separatedBy: ".")
+            if valueArrey[1] == "0" {
+                displayResultLabel.text = "\(valueArrey[0])"
+            } else {
+                displayResultLabel.text = "\(newValue)"
+            }
+            stillTyping = false
+        }
     }
-
-
+    
+    @IBAction func numberPressed(_ sender: UIButton) {
+        let number = sender.currentTitle!
+        if stillTyping {
+            if ((displayResultLabel.text?.count)! <= 10) {
+                displayResultLabel.text = displayResultLabel.text! + number
+            }
+        }
+        else {
+            displayResultLabel.text = number
+            stillTyping = true
+        }
+    }
+    
+    @IBAction func twoOperandsSignPressed(_ sender: UIButton) {
+        operationSign = sender.currentTitle!
+        firstOperand = currentInput
+        stillTyping = false
+        dotInPlaced = false
+    }
+    
+    func operateWithTwoOperands(operation: (Double, Double) -> Double) {
+        currentInput = operation(firstOperand, secondOperand)
+        stillTyping = false
+    }
+    
+    @IBAction func equalitySignPressed(_ sender: UIButton) {
+        if stillTyping {
+            secondOperand = currentInput
+        }
+        
+        dotInPlaced = false
+        
+        switch operationSign {
+        case "+":
+            operateWithTwoOperands {$0 + $1}
+        case "-":
+            operateWithTwoOperands {$0 - $1}
+        case "ｘ":
+            operateWithTwoOperands {$0 * $1}
+        case "÷":
+            operateWithTwoOperands {$0 / $1}
+        default: break
+        }
+    }
+    
+    @IBAction func clearButtonPressed(_ sender: UIButton) {
+        firstOperand = 0
+        secondOperand = 0
+        currentInput = 0
+        displayResultLabel.text = "0"
+        stillTyping = false
+        dotInPlaced = false
+        operationSign = ""
+    }
+    
+    @IBAction func plusMinusButtonPressed(_ sender: UIButton) {
+        currentInput = -currentInput
+    }
+    
+    @IBAction func percentageButtonPressed(_ sender: UIButton) {
+        if firstOperand == 0 {
+            currentInput = currentInput / 100
+        } else {
+            secondOperand = firstOperand * currentInput / 100
+        }
+    }
+    
+    @IBAction func dotButtonPressed(_ sender: UIButton) {
+        if stillTyping && !dotInPlaced {
+            displayResultLabel.text = displayResultLabel.text! + "."
+            dotInPlaced = true
+        } else if !stillTyping && !dotInPlaced {
+            displayResultLabel.text = "0."
+        }
+    }
+    
 }
-
